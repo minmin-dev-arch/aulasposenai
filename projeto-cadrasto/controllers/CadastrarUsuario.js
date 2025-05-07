49a790164dbb2e3e2b3b467e7c2770deffa1f1e0
minmin-dev-arch <iferreiramariano@gmail.com>
2025-05-07 15:57:34 -0300

    Add files via upload

--- projeto-cadrasto/controllers/CadastrarUsuario.js
+++ projeto-cadrasto/controllers/CadastrarUsuario.js
@@ -1,6 +1,11 @@
 function usuario(nome, idade) {
-    console.log("Nome e idade: " + nome + "," + idade );
+    if(!nome || !idade) {
+        console.log("Erro:Nome ou idade inválidos.")
+    } else { 
+    console.log("Nome: " + nome  );
+    console.log("Idade: " + idade);
     console.log("Usuário cadastrado com sucesso!");
+    }
 }
 
-module.exports=usuario
+module.exports = usuario
