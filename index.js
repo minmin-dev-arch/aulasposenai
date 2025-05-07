d77e835d0fbfbd31b33f9c3a330e36a5d53b9ce3
minmin-dev-arch <iferreiramariano@gmail.com>
2025-05-07 16:51:41 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require ('express')
+
+const app = express()
+
+
+app.get('/', (req,res) => {
+    res.send('Servidor Express funcionando!');
+})
+
+app.listen(3000,() => {
+    console.log('Servidor rodando em http://localhost:3000')
+})
