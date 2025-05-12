804f672e6a6f517be39233e9ec0911d837ae9e18
minmin-dev-arch <iferreiramariano@gmail.com>
2025-05-12 16:38:27 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,34 +1,23 @@
-const express = require ('express')
+const express = require('express')
+const app = express ()
 
-const app = express()
 
-app.use(express.json())
+app.get('/exibir',(req,res) => {
+    res.send('Bem-vindo ao servidor Express!')
 
-app.get('/', (req,res) => {
-    res.send('Servidor Express funcionando!');
 })
 
-app.get('/sobre', (req,res) => {
-    res.send('Bem-vindo á página Sobre!');
+app.get('/sobre' , (req,res) => {
+    res.send('Este é um projeto de exemplo com rotas.')
+
 })
 
-app.get('/usuario', (req,res) => {
-    res.send('Iasmin');
+app.get('/contato' , (req,res) => {
+    res.send('Entre em contato pelo e-mail: iferreiramariano@gmail.com')
+
 })
-
-app.post('/usuario', (req,res) => {
-    const nome = req.body.nome
-    const cargo = req.body.cargo
-    res.send("Usuário "  + nome + " criado com sucesso" + "," +  "Com o cargo de: " + cargo);
-})
-
-const curso = ("Programador back-end")
-
-app.get('/senai', (req,res) => {
-    res.send( curso);
-})
-
 
 app.listen(3000,() => {
     console.log('Servidor rodando em http://localhost:3000')
+
 })
